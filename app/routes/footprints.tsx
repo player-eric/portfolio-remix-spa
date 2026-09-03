9f720042fafd6e5e95635a89f924dc5b94db0c97
Player_eric <digimonyan@gmail.com>
2026-09-02 22:38:07 -0400

    Comment out unused code in footprints.tsx
    Comment out imports, meta, links, and default function.

--- app/routes/footprints.tsx
+++ app/routes/footprints.tsx
@@ -1,48 +1,48 @@
-import { LinksFunction, MetaFunction } from "@remix-run/node";
-import ClientOnly from "~/components/ClientOnly";
-import FootPrintMap from "~/components/FootPrintMap.client";
-import configs from "~/configs/configs";
+// import { LinksFunction, MetaFunction } from "@remix-run/node";
+// import ClientOnly from "~/components/ClientOnly";
+// import FootPrintMap from "~/components/FootPrintMap.client";
+// import configs from "~/configs/configs";
 
-import {
-  footPrintsWithPicture,
-  footPrintsWithoutPicture,
-} from "~/configs/footPrints";
+// import {
+//   footPrintsWithPicture,
+//   footPrintsWithoutPicture,
+// } from "~/configs/footPrints";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: `${configs.navBar.name} | Footprints` },
-    {
-      name: "description",
-      content: `${configs.navBar.name} footprints`,
-    },
-    {
-      name: "keywords",
-      content: `personal, website, about, profile, ${configs.navBar.name}, footprints`,
-    },
-    { name: "author", content: configs.navBar.name },
-    { name: "robots", content: "index, follow" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-  ];
-};
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: `${configs.navBar.name} | Footprints` },
+//     {
+//       name: "description",
+//       content: `${configs.navBar.name} footprints`,
+//     },
+//     {
+//       name: "keywords",
+//       content: `personal, website, about, profile, ${configs.navBar.name}, footprints`,
+//     },
+//     { name: "author", content: configs.navBar.name },
+//     { name: "robots", content: "index, follow" },
+//     { name: "viewport", content: "width=device-width, initial-scale=1" },
+//   ];
+// };
 
-export const links: LinksFunction = () => [
-  {
-    rel: "stylesheet",
-    href: "https://unpkg.com/leaflet@1.8.0/dist/leaflet.css",
-  },
-];
+// export const links: LinksFunction = () => [
+//   {
+//     rel: "stylesheet",
+//     href: "https://unpkg.com/leaflet@1.8.0/dist/leaflet.css",
+//   },
+// ];
 
-export default function FootPrints() {
-  return (
-    <>
-      <ClientOnly>
-        {() => (
-          <FootPrintMap
-            footPrintsWithPicture={footPrintsWithPicture}
-            footPrintsWithoutPicture={footPrintsWithoutPicture}
-          ></FootPrintMap>
-        )}
-      </ClientOnly>
-    </>
-  );
-}
+// export default function FootPrints() {
+//   return (
+//     <>
+//       <ClientOnly>
+//         {() => (
+//           <FootPrintMap
+//             footPrintsWithPicture={footPrintsWithPicture}
+//             footPrintsWithoutPicture={footPrintsWithoutPicture}
+//           ></FootPrintMap>
+//         )}
+//       </ClientOnly>
+//     </>
+//   );
+// }
